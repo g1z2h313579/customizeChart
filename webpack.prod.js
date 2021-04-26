8c8b162af047353d1343f308a719a764f75b944b
管智涵 <guanzhihan@MacBook-Pro-2.local>
2021-04-26 14:06:13 +0800

    qwe

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,20 +4,75 @@ const webpack = require('webpack')
 
 module.exports = merge(common, {
 
-mode : "production",
+    mode: "production",
+    module: {
 
-optimization : {
-    minimize : true,
-    usedExports : true,
-    concatenateModules : true,
-},
+        rules: [
 
-plugins: [
+            {
+                test: /\.s[ac]ss$/i,
 
-new webpack.DefinePlugin({
-"process.env.NODE_ENV" : "production"
-})
-],
-devtool : 'none'
+                use: ["style-loader", "css-loader", "sass-loader"]
+
+            },
+            {
+                test: /\.css$/i,
+
+                use: ["style-loader", "css-loader"]
+
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[path][name].[ext]',
+                            outputPath: './img'
+                        }
+                    }
+                ],
+            },
+            {
+                test: /\.(js|jsx)$/,
+
+                use: {
+
+                    loader: 'babel-loader',
+
+                    options: {
+
+                        presets: ['@babel/preset-env', '@babel/preset-react', 'mobx'],
+
+                    }
+                },
+                exclude: /node_modules/
+
+            },
+            {
+                test: /\.(tsx|ts)?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+
+        extensions: [".js", ".jsx",'.ts', '.tsx']
+
+    },
+    optimization: {
+        minimize: true,
+        usedExports: true,
+        concatenateModules: true,
+    },
+
+    plugins: [
+        new webpack.NamedModulesPlugin(),
+        new webpack.DefinePlugin({
+            "process.env.NODE_ENV": "production"
+        })
+    ],
+    devtool: 'none'
 
 });
