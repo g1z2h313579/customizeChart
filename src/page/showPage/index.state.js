976c5595c8074899d7675286470a173e9a8e0636
管智涵 <guanzhihan@MacBook-Pro-2.local>
2021-05-06 17:50:12 +0800

    showpage改时间，一层变更数据

--- src/page/showPage/index.state.js
+++ src/page/showPage/index.state.js
@@ -26,6 +26,9 @@ export default new class {
         let selectTargetList = []
         let chartDatatype = []
         let targetNameList = []
+
+        
+
         this.pageData.map(v => {
             v.pageData.data.map(item => {
                 chartDatatype.push(item.chartDatatype)
@@ -37,15 +40,13 @@ export default new class {
         for(let i = 0; i < selectTargetList.length; i++) {
             tempData.push(await axios.get(`/data/2021/${targetToUrl[selectTargetList[i]]}`))  
         }
-
+        console.log("tempData",tempData)
         this.treatChartData(tempData,chartDatatype,targetNameList)
         
     }
 
     @action treatChartData = (data, chartDatatype,targetNameList) => {
-        // console.log("data",data)
-        // console.log("chartDatatype",chartDatatype)
-        // console.log("targetNameList",targetNameList)
+        console.log("chartDatatype",chartDatatype)
         let dataStep = chartDatatype.map(v => {
             let t = 0
             if(v === 'single'){
@@ -55,12 +56,7 @@ export default new class {
             }
             return t
         })
-        // [{},{},{}]
-        // [2,1]
-
-        // [{},{},{},{},{},{}]
-        // [1,2,2,1]
-        // console.log("dataStep",dataStep)
+ 
         let tmpData = []
         for(let i = 0; i < dataStep.length; i++){
             if(i === 0){
@@ -73,18 +69,15 @@ export default new class {
                         targetNameList[dataStep[i] - 2],
                         targetNameList[dataStep[i] - 1],
                     ]
-                    // console.log("ttttttt0000",t)
-                    // console.log("qqqqqq0000",q)
+  
                     let a = doubleDataMerge(t,q)
                     tmpData.push(a)
-                    // console.log("aaaa0000",a)
+                   
                 }else {
-                    
-                    // console.log("targetNameList[dataStep[i] - 1]",targetNameList[dataStep[i] - 1])
-                    // console.log("data[dataStep[i] - 1].data.data[this.date.yearMonth]",data[dataStep[i] - 1].data.data[this.date.yearMonth])
                     let b = singleData(data[dataStep[i] - 1].data.data[this.date.yearMonth], targetNameList[dataStep[i] - 1])
+                    // console.log("bbbb1111",b)
                     tmpData.push(b)
-                    // console.log("bbbbbb",b)
+                    
                 }
             }else {
                 let dataIndex = -1
@@ -101,18 +94,15 @@ export default new class {
                         targetNameList[dataIndex - 1],
                         targetNameList[dataIndex],
                     ]
-                    // console.log("ttttttt",t)
-                    // console.log("qqqqqq",q)
+                    
                     let a = doubleDataMerge(t,q)
                     tmpData.push(a)
-                    // console.log("aaaa",a)
+                  
                 }else {
-                    
-                    // console.log("3333",targetNameList[dataStep[i] - 1])
-                    // console.log("4444",data[dataStep[i] - 1].data.data[this.date.yearMonth])
                     let b = singleData(data[dataIndex].data.data[this.date.yearMonth], targetNameList[dataIndex])
+                    // console.log("bbbb2222",b)
                     tmpData.push(b)
-                    // console.log("bbbbbb3333",b)
+                    
                 }
             }
             
@@ -123,8 +113,36 @@ export default new class {
                 item.data = tmpData[index]
             })
         })
-        this.currentPageInfo = this.pageData.filter(v => v.pageName === this.currentPageInfo.pageName)[0].pageData
+
         // console.log("this.pageData11111",toJS(this.pageData))
         // console.log("this.currentPageInfo",toJS(this.currentPageInfo))
+        // console.log("this.findCurrentPageInfo(this.pageData)",this.findCurrentPageInfo(this.pageData))
+        let t = this.findCurrentPageInfo(toJS(this.pageData))
+        console.log("tttt",t)
+        this.currentPageInfo = t
+        // this.currentPageInfo = this.pageData.filter(v => v.pageName === this.currentPageInfo.pageName)[0].pageData
+    }
+
+    @action findCurrentPageInfo = (data) => {
+        console.log("data",data)
+        let t = null
+        for(let i = 0; i < data.length; i++){
+            t = this.findCPIFn(data[i])
+            if(t) return t
+        }
+        
+    }
+
+
+    @action findCPIFn = (item) => {
+        if(item.pageName === this.currentPageInfo.pageName){
+            return item.pageData
+        }else {
+            if(item.children && item.children.length > 0){
+                return this.findCurrentPageInfo(item.children)
+            }else {
+                return null
+            }
+        }
     }
 }
